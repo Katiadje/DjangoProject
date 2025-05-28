dd5eb3ecdff995c83c2bc2aa7fc15d8205b235ee
Katiadje <katiadjellali05@gmail.com>
2025-05-28 14:41:33 +0200

    commit changes

--- django_projet/settings.py
+++ django_projet/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'django_projet.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'sakila',
+        'USER': 'root',
+        'PASSWORD': 'Hitema2025',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
--- sakila/urls.py
+++ sakila/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('banner/', views.banner, name='banner'),
     path('countries/', views.countries, name='countries'),
     path('cities/', views.cities, name='cities'),
-    path('banner/', views.banner, name='banner'),
-    path('films/', views.films, name='films'),  # ajout pour films
+    path('films/', views.films, name='films'),
 ]
